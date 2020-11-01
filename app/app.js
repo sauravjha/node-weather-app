998b85f10748c8689290cc8776227d963fb1110c
Saurav Jha <sauravjha24@gmail.com>
2020-11-01 16:15:04 +1100

    Learning templeting CSS styling etc

--- app/app.js
+++ app/app.js
@@ -6,41 +6,6 @@ const request = require("request");
 
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
-// const url = "http://api.weatherstack.com/current?access_key=e6ffb9a32a68e38475971a2c74b72021&query=Melbourne"
-
-// request({url: url, json: true}, (error, response) =>{
-//   if(!error) {
-//     if(!response.body.error) {
-//       console.log(response.statusCode)
-//       const temperature = response.body.current.temperature
-//       const feelslike  = response.body.current.feelslike
-//       const rain  = response.body.current.precip
-//       const weather_description = response.body.current.weather_descriptions[0]
-//       console.log(weather_description + ". It is currently ", temperature, "degree out. Feels like", feelslike,"degree out.")
-//       console.log(weather_description + ". It is currently ", temperature, "degree out. There is", rain,"% chances of rain")
-//     } else {
-//       console.log(chalk.red("Some issue with the request url"))
-//     }
-//   } else {
-//     console.log("Unable to connect to the weatherstack")
-//   }
-// })
-
-
-// const geoLocation = "https://api.mapbox.com/geocoding/v5/mapbox.places/unit 2, 127 kitchener Street Broadmedows.json?access_token=pk.eyJ1Ijoic2F1cmF2amhhMjQiLCJhIjoiY2tneGYzMWhzMGJtcTJycnNyNnprN2g4dSJ9.jQvfZP5ATORRMRMMxSD0cg"
-
-// request({url: geoLocation, json: true}, (error, response) =>{
-//   if(!error) {
-//     if(!response.body.error){
-//       const longitudeLatitude = response.body.features[0].geometry.coordinates
-//       console.log(longitudeLatitude[0], longitudeLatitude[1])
-//     } else {
-//       console.log(chalk.red("Some issue with the request url"))
-//     }
-//   } else {
-//     console.log(chalk.red("Unable to connect map box"))
-//   }
-// })
 
 var address = process.argv[2]
 
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 const app = express()
 
 console.log(__dirname)
@@ -8,11 +8,45 @@ console.log(__filename)
 console.log(path.join(__dirname, '../public'))
 
 const publicDir = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+// seting the view path
+app.set('views', viewPath)
+//register partials
+hbs.registerPartials(partialsPath)
+
 
 app.use(express.static(publicDir))
 
-app.get('', (req, res)=>{
-    res.send("<h1>Hello from Express server</h1>")
+app.get('/hbs', (req, res)=>{
+    res.render('index', {
+        title: "Weather App",
+        name: 'Kaira Jha'
+    })
+})
+
+app.get('/hbs/about', (req, res)=>{
+    res.render('about', {
+        title: "About me",
+        name: 'Kaira Jha'
+    })
+})
+app.get('/hbs/help', (req, res)=>{
+    res.render('help', {
+        title: "Help",
+        message: "This is where you put your help message",
+        name: 'Kaira Jha'
+    })
+})
+
+app.get('/hbs/help/*', (req, res)=>{
+    res.render('help', {
+        title: "404",
+        message: "Page not found",
+        name: 'Kaira Jha'
+    })
 })
 
 app.get('/help', (req, res)=>{
@@ -24,12 +58,20 @@ app.get('/about', (req, res)=>{
 })
 
 app.get('/weather', (req, res)=>{
-    // console.log(res)
     res.send({
         forcast: "Partly Cloudy",
         location: "Broadmedows"
     })
 })
+
+app.get('*', (req, res)=>{
+    res.render('help', {
+        title: "Help",
+        message: "404 Help artical not found",
+        name: 'Kaira Jha'
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("Server is running in port 3000")
 })
